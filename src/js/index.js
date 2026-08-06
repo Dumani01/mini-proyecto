7d36b0d94fd22a99aa0a6659d2d3e2a29dbf5978
Dumani01 <bernnydumanifwd@gmail.com>
2026-08-06 14:41:49 -0600

    modulo login

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+// Crear usuario por primera vez
+
+if (!localStorage.getItem("usuario")) {
+
+    localStorage.setItem("usuario", "admin");
+    localStorage.setItem("password", "12345");
+
+}
+
+const formulario = document.getElementById("loginForm");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    const usuarioGuardado = localStorage.getItem("usuario");
+    const passwordGuardada = localStorage.getItem("password");
+
+    const mensaje = document.getElementById("mensaje");
+
+    if(usuario === usuarioGuardado && password === passwordGuardada){
+
+        localStorage.setItem("logueado", "true");
+
+        window.location.href = "dashboard.html";
+
+    }else{
+
+        mensaje.textContent = "Usuario o contraseña incorrectos";
+
+    }
+
+});
